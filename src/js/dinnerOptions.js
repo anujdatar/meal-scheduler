014430aced878d32b84a11008b00830ef501edc3
Anuj Datar <anuj.datar@gmail.com>
2018-10-09 19:52:39 -0400

    functions to compare additions with local db entries

--- src/js/dinnerOptions.js
+++ src/js/dinnerOptions.js
@@ -1,10 +1,12 @@
+import axios from 'axios'
 
-// parse input csv or txt file
+
+// ***************** reading files ***********************************
 function readCSV (textFile) {
+  // parse input csv or txt file
   const lines = textFile.split(/\r\n|\n/)
 
   let items = []
-
   for (let i=0; i<lines.length; i++) {
     if (lines[i].length > 0) {
       let itemsRow = lines[i].split(',')
@@ -18,9 +20,8 @@ function readCSV (textFile) {
   return items
 }
 
-// handle file upload - file containing menu items
 export function getNewMenuList () {
-
+  // handle file upload - file containing menu items
   let fileSelector = document.getElementById("addFile")
   let file = fileSelector.files[0]
   if (!file) {
@@ -41,15 +42,114 @@ export function getNewMenuList () {
     return false
   }
 }
+// *******************************************************************
 
-// handle single additions to menu list
+
+// ********** string operations ***************************************
+function removeCharactersFromString(string, filterItems) {
+  // remove unwanted characters for string compare
+  filterItems.forEach(filter => {
+    while(string.indexOf(filter) > -1) {
+      string = string.replace(filter, "")
+    }
+  })
+  return string
+}
+
+function isItemInList (list, newEntry) {
+  // check if a string item exists in a list
+
+  // transform elements of list for compare
+  let modifiedList = []
+  const filters = [" ", "'", "-", ","]
+  list.forEach(item => {
+    item = removeCharactersFromString(item, filters)
+    modifiedList.push(item.toLowerCase())
+  })
+  
+  // transform newly entered item
+  newEntry = removeCharactersFromString(newEntry, filters)
+  newEntry = newEntry.toLowerCase()
+
+  console.log(modifiedList)
+  console.log(newEntry)
+
+  if (modifiedList.indexOf(newEntry) > -1) {
+    console.log('old item')
+    return true
+  }
+  console.log('new item')
+  return false
+}
+// ********************************************************************
+
+
+// *************** add a single item to the db ************************
+function writeToDb (newEntry) {
+  // read from db
+  axios.get('http://localhost:3000/getListOfItems')
+  .then (response => {
+    // make list of names from db entries
+    let existingList = []
+    const dbEntries = response.data
+    dbEntries.forEach( dbEntry => {
+      existingList.push(dbEntry.name)
+    })
+    // check if item is in list
+    if (!isItemInList(existingList, newEntry)) {
+      console.log('new item, add it to the db')
+    } else {
+      console.log('item in db already, do nothing')
+    }
+
+  })
+}
+// ********************************************************************
+
+
+// ********************************************************************
 export function getNewMenuItem () {
+  // handle single additions to menu list
   const newItem = document.getElementById("newItem")
   if (newItem.value) {
-    // do something
-    console.log(newItem.value)
-
-    // reset field value
+  
+    // read and reset field value
+    const newItemName = newItem.value
     newItem.value = ''
+
+    // write to db
+    writeToDb(newItemName)
+
   }
 }
+// ********************************************************************
+
+
+
+// .then (response => {
+    //   const jsonData = response.data
+    //   jsonData.forEach(doc => {
+    //     if (newItemName.toUpperCase() === doc.name.toUpperCase()) {
+    //       console.log('same')
+    //       return false
+    //     } else {
+    //       console.log('new item')
+    //       let newDoc = {
+    //         name: newItemName,
+    //         selected: true
+    //       }
+    //       console.log(newDoc)
+
+    //       axios.post('http://localhost:3000/addItemToDb', newDoc)
+    //         .then(resp => {
+    //           console.log(resp.data)
+    //           console.log('maybe')
+    //         })
+    //         .catch(function (error) {
+    //           console.log(error)
+    //         })
+    //     }
+
+
+    //   })
+    // })
--- src/js/localDbInteract.js
+++ src/js/localDbInteract.js
@@ -1,44 +1,38 @@
 import axios from 'axios'
 
 // render data sent from db server
-export function renderMenuItem (ul, doc) {
+function renderMenuItem (ul, doc) {
+
+  // define HTML elements
   let li = document.createElement('li')
-
   let checkbox = document.createElement('input')
-  checkbox.type = 'checkbox'
-  checkbox.checked = doc.selected
-
   let name = document.createElement('span')
   let removeBtn = document.createElement('button')
 
+  // assign element properties and data
   li.setAttribute('data-id', doc._id)
+  checkbox.type = 'checkbox'
+  checkbox.checked = doc.selected
   name.textContent = doc.name
   removeBtn.textContent = 'Remove'
 
+  // attach all elements to the HTML list
   li.appendChild(checkbox)
   li.appendChild(name)
   li.appendChild(removeBtn)
-
   ul.appendChild(li)
 }
 
+
 export function renderMenuList () {
   const menuList = document.getElementById('menuList')
+  menuList.innerHTML = ''
 
   axios.get('http://localhost:3000/getListOfItems')
   .then (response => {
     const jsonData = response.data
-    // console.log(jsonData)
     jsonData.forEach(doc => {
-      // console.log(doc._id)
-      // console.log(doc.name)
-      // console.log(doc.selected)
       renderMenuItem(menuList, doc)
     })
   })
-}
-
-// write to database
-export function writeLocalDb (doc) {
-  console.log(doc)
 }
