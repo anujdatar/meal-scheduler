b28544a2c5b76ab1d269064362aa8de8e3c1d230
Anuj Datar <anuj.datar@gmail.com>
2018-11-17 05:11:22 -0500

    adding file uploader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule,
+  MatIconModule, MatListModule, MatFormFieldModule, MatInputModule,
+  MatSelectModule } from '@angular/material';
 import { HomepageComponent } from './homepage/homepage.component';
 import { AboutComponent } from './about/about.component';
 import { DinnerOptionsComponent } from './dinner-options/dinner-options.component';
@@ -28,7 +30,10 @@ import { DinnerOptionsComponent } from './dinner-options/dinner-options.componen
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
