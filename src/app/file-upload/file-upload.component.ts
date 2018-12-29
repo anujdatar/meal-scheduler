0ee2c9a06cc9f5e470eb8297983cd7c294cb017f
Anuj Datar <anuj.datar@gmail.com>
2018-12-29 08:02:02 -0500

    adding an angular like file upload widget

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit, NgModule } from '@angular/core';
 })
 export class FileUploadComponent implements OnInit {
 
-  constructor() { }
+  readFname: any;
 
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.readFname = function() {
+      const file = (<HTMLInputElement>document.getElementById('uploadFile')).files[0];
+      if (file) {
+        console.log(file.name);
+        const fileLabel = document.getElementById('uploadFilename');
+        const fileSizeStr = fileSizeCheck(file.size);
+        fileLabel.innerHTML = file.name + ' | ' + fileSizeStr;
+      } else {
+        console.log('no file selected');
+      }
+    };
+
+    function fileSizeCheck(inputFileSize) {
+      let fileSize: number = inputFileSize;
+      const sizeUnits = ['Bytes', 'KB', 'MB'];
+      let starter = 0;
+      while (fileSize < 1024) {
+        fileSize = fileSize / 1024;
+        starter += starter;
+      }
+      const returnString = fileSize + ' ' + sizeUnits[starter];
+      return returnString;
+    }
+  }
 }
